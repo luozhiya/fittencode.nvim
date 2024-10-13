841423dc0926b1c69d395dd6abc91eb313fadc94
luozhiya <luozhiya@petalmail.com>
2024-10-13 19:04:41 +0800

    Clean

--- lua/fittencode/integration/init.lua
+++
@@ -1,7 +0,0 @@
-local Config = require('fittencode.config')
-
-if Config.integration.completion.enable then
-    if Config.integration.completion.engine == 'cmp' then
-        require('fittencode.integration.cmp').register_source()
-    end
-end
