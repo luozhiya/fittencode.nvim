e08147308f87803538a7e2414548f3a929624691
luozhiya <luozhiya@petalmail.com>
2025-02-07 14:02:37 +0800

    3

--- lua/fittencode/containers/unordered_map.lua
+++ lua/fittencode/containers/unordered_map.lua
@@ -1,72 +1,80 @@
-local M = {}
+local UnorderedMap = {}
+UnorderedMap.__index = UnorderedMap
 
--- Unordered Map（基于哈希表）
-function M.UnorderedMap()
+function UnorderedMap:new()
     local map = {
         _data = {},
         _keys = {}, -- 维护插入顺序
         _size = 0
     }
-
-    local methods = {
-        size = function(self) return self._size end,
-        empty = function(self) return self._size == 0 end,
-
-        insert = function(self, key, value)
-            if not self._data[key] then
-                table.insert(map._keys, key)
-                self._size = self._size + 1
-            end
-            self._data[key] = value
-        end,
-
-        erase = function(self, key)
-            if self._data[key] then
-                self._data[key] = nil
-                for i = #map._keys, 1, -1 do
-                    if map._keys[i] == key then
-                        table.remove(map._keys, i)
-                        break
-                    end
-                end
-                self._size = self._size - 1
-            end
-        end,
-
-        find = function(self, key)
-            return self._data[key]
-        end,
-
-        contains = function(self, key)
-            return self._data[key] ~= nil
-        end,
-
-        clear = function(self)
-            self._data = {}
-            self._keys = {}
-            self._size = 0
-        end,
-
-        -- 支持无序迭代
-        items = function(self)
-            local i = 0
-            return function()
-                i = i + 1
-                local key = map._keys[i]
-                if key then
-                    return key, self._data[key]
-                end
-            end
-        end
-    }
-
-    local mt = {
-        __index = methods,
-        __len = function(self) return self._size end,
-        __pairs = function(self) return self:items() end
-    }
-
-    return setmetatable(map, mt)
+    setmetatable(map, UnorderedMap)
+    return map
 end
 
-return M
+function UnorderedMap.is_unordered_map(obj)
+    return type(obj) == 'table' and getmetatable(obj) == UnorderedMap
+end
+
+function UnorderedMap:size()
+    return self._size
+end
+
+function UnorderedMap:empty()
+    return self._size == 0
+end
+
+function UnorderedMap:insert(key, value)
+    if not self._data[key] then
+        table.insert(self._keys, key)
+        self._size = self._size + 1
+    end
+    self._data[key] = value
+end
+
+function UnorderedMap:erase(key)
+    if self._data[key] then
+        self._data[key] = nil
+        for i = #self._keys, 1, -1 do
+            if self._keys[i] == key then
+                table.remove(self._keys, i)
+                break
+            end
+        end
+        self._size = self._size - 1
+    end
+end
+
+function UnorderedMap:find(key)
+    return self._data[key]
+end
+
+function UnorderedMap:contains(key)
+    return self._data[key] ~= nil
+end
+
+function UnorderedMap:clear()
+    self._data = {}
+    self._keys = {}
+    self._size = 0
+end
+
+function UnorderedMap:items()
+    local i = 0
+    return function()
+        i = i + 1
+        local key = self._keys[i]
+        if key then
+            return key, self._data[key]
+        end
+    end
+end
+
+function UnorderedMap:__len()
+    return self._size
+end
+
+function UnorderedMap:__pairs()
+    return self:items()
+end
+
+return UnorderedMap
--- lua/fittencode/containers/vector.lua
+++ lua/fittencode/containers/vector.lua
@@ -1,102 +1,115 @@
-local M = {}
+local Vector = {}
+Vector.__index = Vector
 
--- Vector（动态数组）
-function M.Vector()
+function Vector:new()
     local vec = {
         _data = {},
         _size = 0,
         _capacity = 0
     }
 
-    local function ensure_capacity(new_size)
-        if new_size > vec._capacity then
-            local new_cap = math.max(vec._capacity * 2, new_size)
-            local new_data = {}
-            for i = 1, vec._size do
-                new_data[i] = vec._data[i]
-            end
-            vec._data = new_data
-            vec._capacity = new_cap
-        end
-    end
+    setmetatable(vec, Vector)
 
-    local methods = {
-        size = function(self) return self._size end,
-        capacity = function(self) return self._capacity end,
-        empty = function(self) return self._size == 0 end,
-
-        at = function(self, index)
-            if index < 1 or index > self._size then
-                error(string.format('Vector index out of range [%d]', index))
-            end
-            return self._data[index]
-        end,
-
-        push_back = function(self, value)
-            ensure_capacity(self._size + 1)
-            self._size = self._size + 1
-            self._data[self._size] = value
-        end,
-
-        pop_back = function(self)
-            if self._size == 0 then error('Vector is empty') end
-            local val = self._data[self._size]
-            self._data[self._size] = nil
-            self._size = self._size - 1
-            return val
-        end,
-
-        insert = function(self, pos, value)
-            if pos < 1 or pos > self._size + 1 then
-                error('Invalid insert position')
-            end
-            ensure_capacity(self._size + 1)
-            table.insert(self._data, pos, value)
-            self._size = self._size + 1
-        end,
-
-        erase = function(self, pos)
-            if pos < 1 or pos > self._size then
-                error('Invalid erase position')
-            end
-            table.remove(self._data, pos)
-            self._size = self._size - 1
-        end,
-
-        clear = function(self)
-            self._data = {}
-            self._size = 0
-            self._capacity = 0
-        end,
-
-        reserve = function(self, new_cap)
-            if new_cap > self._capacity then
-                local new_data = {}
-                for i = 1, self._size do
-                    new_data[i] = self._data[i]
-                end
-                self._data = new_data
-                self._capacity = new_cap
-            end
-        end,
-
-        -- 支持Lua风格迭代
-        ipairs = function(self)
-            return coroutine.wrap(function()
-                for i = 1, self._size do
-                    coroutine.yield(i, self._data[i])
-                end
-            end)
-        end
-    }
-
-    local mt = {
-        __index = methods,
-        __len = function(self) return self._size end,
-        __ipairs = function(self) return self:ipairs() end
-    }
-
-    return setmetatable(vec, mt)
+    return vec
 end
 
-return M
+local function ensure_capacity(vec, new_size)
+    if new_size > vec._capacity then
+        local new_cap = math.max(vec._capacity * 2, new_size)
+        local new_data = {}
+        for i = 1, vec._size do
+            new_data[i] = vec._data[i]
+        end
+        vec._data = new_data
+        vec._capacity = new_cap
+    end
+end
+
+function Vector.is_vector(obj)
+    return type(obj) == 'table' and getmetatable(obj) == Vector
+end
+
+function Vector:size()
+    return self._size
+end
+
+function Vector:capacity()
+    return self._capacity
+end
+
+function Vector:empty()
+    return self._size == 0
+end
+
+function Vector:at(index)
+    if index < 1 or index > self._size then
+        error(string.format('Vector index out of range [%d]', index))
+    end
+    return self._data[index]
+end
+
+function Vector:push_back(value)
+    ensure_capacity(self, self._size + 1)
+    self._size = self._size + 1
+    self._data[self._size] = value
+end
+
+function Vector:pop_back()
+    if self._size == 0 then error('Vector is empty') end
+    local val = self._data[self._size]
+    self._data[self._size] = nil
+    self._size = self._size - 1
+    return val
+end
+
+function Vector:insert(pos, value)
+    if pos < 1 or pos > self._size + 1 then
+        error('Invalid insert position')
+    end
+    ensure_capacity(self, self._size + 1)
+    table.insert(self._data, pos, value)
+    self._size = self._size + 1
+end
+
+function Vector:erase(pos)
+    if pos < 1 or pos > self._size then
+        error('Invalid erase position')
+    end
+    table.remove(self._data, pos)
+    self._size = self._size - 1
+end
+
+function Vector:clear()
+    self._data = {}
+    self._size = 0
+    self._capacity = 0
+end
+
+function Vector:reserve(new_cap)
+    if new_cap > self._capacity then
+        local new_data = {}
+        for i = 1, self._size do
+            new_data[i] = self._data[i]
+        end
+        self._data = new_data
+        self._capacity = new_cap
+    end
+end
+
+function Vector:ipairs()
+    return coroutine.wrap(function()
+        for i = 1, self._size do
+            coroutine.yield(i, self._data[i])
+        end
+    end)
+end
+
+function Vector:__len()
+    return self._size
+end
+
+function Vector:__ipairs()
+    return self:ipairs()
+end
+
+return Vector
