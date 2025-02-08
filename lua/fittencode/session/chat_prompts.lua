2c9367178c545ef0bb89aba9bb32105b27f08100
luozhiya <luozhiya@petalmail.com>
2025-02-08 18:05:21 +0800

    1

--- lua/fittencode/session/chat_prompts.lua
+++ lua/fittencode/session/chat_prompts.lua
@@ -1,3 +1,5 @@
+local VM = require('fittencode.vm')
+
 local M = {}
 
 ---@param text? string|string[]
@@ -9,26 +11,37 @@ function M.word_segmentation(text)
         fmttext = text
     end
     assert(#fmttext > 0, 'text should not be empty')
-    local lines = ''
+    local messages = {}
     for idx, t in ipairs(fmttext) do
+        local lines = ''
         lines = lines .. '# ' .. idx .. '\n'
         lines = lines .. '\n```\n'
         lines = lines .. t .. '\n'
         lines = lines .. '```\n\n'
+        messages[idx] = {
+            content = lines,
+        }
     end
-    local inputs = (
-        '<|system|>\n' ..
-        'Please reply directly to the code without any explanation.\n' ..
-        'Please do not use markdown when replying.\n' ..
-        '请完全使用中文回答。\n<|end|>\n' ..
-        '<|user|>\n' ..
-        lines ..
-        '\n\n' ..
-        '请对上述这几段原文分别进行语义分词（Incorporate Semantic），按如下要求来执行：' ..
-        '1. 输出json格式的分词向量，一级标题作为key，json为一个完整对象' ..
-        '2. 严禁对原文字符做任何修改，包括标点符号。分词结果concat合并起来要和原文完全相等' ..
-        '\n\n\n<|end|>\n<|assistant|>'
-    )
+    local template = [[<|system|>
+Please reply directly to the code without any explanation.
+Please do not use markdown when replying.
+请完全使用中文回答。
+<|end|>
+<|user|>
+请对下面的几段原文分别进行语义分词（Incorporate Semantic），按如下2个要求来执行：
+1. 严禁对原文字符做任何修改，包括标点符号。分词结果concat合并起来要和原文完全相等
+2. 输出json格式的分词向量，一级标题作为key，json为一个完整对象
+
+{{#each messages}}
+{{content}}
+{{/each}}
+
+<|end|>
+<|assistant|>]]
+    local env = {
+        messages = messages,
+    }
+    local inputs = assert(VM:new():run(env, template))
     return {
         inputs = inputs,
         ft_token = M.get_ft_token(),
