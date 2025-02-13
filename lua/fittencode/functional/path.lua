2a6711b64dc83cb2ac51f57ad7d8149e63a4e61b
luozhiya <luozhiya@petalmail.com>
2025-02-14 07:53:05 +0800

    c 66

--- lua/fittencode/functional/path.lua
+++ lua/fittencode/functional/path.lua
@@ -9,33 +9,6 @@ Path 模块的设计区别于 Node.js 的模块，可以提供更加灵活的接
 - 路径可以智能拼接
 - 路径可以智能解析
 
-local p = M
-
----------------------------------------
--- 使用 new/windows/posix 方法创建路径对象
----------------------------------------
-
-print(p.new('', 'windows'):join('C:\\Program Files'))    -- C:\Program Files
-print(p.windows():join('C:\\Program Files'):to('posix')) -- 输出 C:/Program Files
-print(p.windows('C:\\Program Files'):to('posix'))        -- 输出 C:/Program Files
-print(p.windows('C:\\Program Files'):flip_slashes())     -- 输出 C:/Program Files
-
----------------------------------------
--- 混合平台路径操作
----------------------------------------
-
-local project_path = p.new('src/components', 'posix')
-    :to('windows')
-    :join('..\\utils')
-    :normalize()
-print(project_path) -- 输出 src\utils
-
-local res = p.new('/usr/local')
-    :join('bin/neovim')
-    :join('../share/nvim/runtime')
-    :normalize()
-print(res) -- 输出 /usr/local/bin/share/nvim/runtime
-
 --]]
 
 local Platform = require('fittencode.functional.platform')
