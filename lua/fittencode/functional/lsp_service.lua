0901bb230825f1e96a652f0124960e6704801475
luozhiya <luozhiya@petalmail.com>
2025-02-14 11:32:10 +0800

    c 70

--- lua/fittencode/functional/lsp_service.lua
+++ lua/fittencode/functional/lsp_service.lua
@@ -1,26 +1,6 @@
 ---@class FittenCode.Inline.LspService
 local M = {}
 
--- -- LSP 管理器
--- local LSPManager = {}
--- LSPManager.__index = LSPManager
-
--- function LSPManager.new()
---     return setmetatable({
---         installed_servers = {},
---         language_map = {
---             python = 'pyright',
---             lua = 'sumneko_lua',
---             -- 添加更多语言映射...
---         }
---     }, LSPManager)
--- end
-
--- function LSPManager:check_installed(lang)
---     local server = self.language_map[lang]
---     return self.installed_servers[server] ~= nil
--- end
-
 function M.check_installed(lang)
 end
 
