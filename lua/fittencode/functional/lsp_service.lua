16412f00dd2ff8d4839fe1bf4b03df32c7e40dad
luozhiya <luozhiya@petalmail.com>
2025-02-19 17:48:59 +0800

    17

--- lua/fittencode/functional/lsp_service.lua
+++ lua/fittencode/functional/lsp_service.lua
@@ -8,4 +8,19 @@ end
 function M.async_notify_install_lsp(buf)
 end
 
+function M.has_lsp_client(buf)
+    local clients = vim.lsp.get_clients(buf)
+    return #clients > 0
+end
+
+function M.supports_method(buf, method)
+    local clients = vim.lsp.get_clients(buf)
+    for _, client in ipairs(clients) do
+        if client:supports_method(method) then
+            return true
+        end
+    end
+    return false
+end
+
 return M
