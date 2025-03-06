e42ee3d2f7dcbd2b89d05cdb602e59b02641b0b6
luozhiya <luozhiya@petalmail.com>
2025-03-06 08:41:00 +0800

    87

--- lua/fittencode/chat/conversation_types_provider.lua
+++ lua/fittencode/chat/conversation_types_provider.lua
@@ -66,37 +66,7 @@ end
 
 function ConversationTypesProvider:load_builtin_templates()
     Log.debug('ConversationTypesProvider will load built-in templates based on extension URI: {}', self.extension_uri)
-    local list = {
-        chat = {
-            'chat-en.rdt.md',
-            'chat-zh-cn.rdt.md'
-        },
-        task = {
-            'diagnose-errors-en.rdt.md',
-            'diagnose-errors-zh-cn.rdt.md',
-            'diagnose-errors.rdt.md',
-            'document-code-en.rdt.md',
-            'document-code-zh-cn.rdt.md',
-            'edit-code-en.rdt.md',
-            'edit-code-zh-cn.rdt.md',
-            'explain-code-en.rdt.md',
-            'explain-code-w-context.rdt.md',
-            'explain-code-zh-cn.rdt.md',
-            'find-bugs-en.rdt.md',
-            'find-bugs-zh-cn.rdt.md',
-            'generate-code-en.rdt.md',
-            'generate-code-zh-cn.rdt.md',
-            'generate-unit-test-en.rdt.md',
-            'generate-unit-test-zh-cn.rdt.md',
-            'improve-readability.rdt.md',
-            'optimize-code-en.rdt.md',
-            'optimize-code-zh-cn.rdt.md',
-            'terminal-fix-en.rdt.md',
-            'terminal-fix-zh-cn.rdt.md',
-            'title-chat-en.rdt.md',
-            'title-chat-zh-cn.rdt.md',
-        }
-    }
+    local list = require('fittencode.chat.builtin_templates')
     for k, v in pairs(list) do
         for _, file in ipairs(v) do
             local ct = self:load_builtin_template(k, file)
