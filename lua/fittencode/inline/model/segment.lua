94492d8907fbd01dc102f334a542861f2046b1b9
luozhiya <luozhiya@petalmail.com>
2025-05-21 17:19:45 +0800

    2025/05/21

--- lua/fittencode/inline/model/segment.lua
+++ lua/fittencode/inline/model/segment.lua
@@ -44,4 +44,31 @@ function M.segments_to_words(model, segments)
     return words
 end
 
+-- 高级分词
+---@return FittenCode.Concurrency.Promise, FittenCode.HTTP.Response?
+function M.send_segments(text)
+    if Editor.onlyascii(text) then
+        Log.debug('Generated text is only ascii, skip word segmentation')
+        return Promise.resolve()
+    end
+
+    local request = Client.make_request(Protocol.Methods.chat_auth, {
+        body = assert(vim.fn.json_encode(Segmentation.generate(text))),
+    })
+    if not request then
+        Log.error('Failed to send request')
+        return Promise.reject()
+    end
+
+    return request:async():forward(function(response)
+        local segments = response.json()
+        if segments then
+            return segments
+        else
+            Log.error('Failed to parse: {}', response)
+            return Promise.reject()
+        end
+    end), request
+end
+
 return M
