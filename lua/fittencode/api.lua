c9b7e846ad9a92636fbaa10f5f1b84b2b984c658
luozhiya <luozhiya@petalmail.com>
2025-05-26 09:40:54 +0800

    2025/05/26

--- lua/fittencode/api.lua
+++ lua/fittencode/api.lua
@@ -5,7 +5,7 @@ API函数列表：
 - has_suggestions()：检查是否有关于补全的建议
 - dismiss_suggestions()：撤销补全建议
 - accept_all_suggestions()：接受所有补全建议
-- accept_line()：接受当前行补全建议
+- accept_next_line()：接受当前行补全建议
 - revoke_line()：撤销当前行补全建议
 - accept_next_word()：接受当前单词补全建议
 - revoke_word()：撤销当前单词补全建议
