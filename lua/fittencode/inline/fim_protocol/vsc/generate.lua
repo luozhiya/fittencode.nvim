1f6c8af25f59ad37f769cd9296a95ca64f83ed3e
luozhiya <luozhiya@petalmail.com>
2025-05-31 04:10:01 +0800

    2025/05/31

--- lua/fittencode/inline/fim_protocol/vsc/generate.lua
+++ lua/fittencode/inline/fim_protocol/vsc/generate.lua
@@ -51,49 +51,39 @@ local function compute_text_diff_metadata(current_text, current_cipher, filename
 
     ---@type table<number>
     ---@diagnostic disable-next-line: assign-type-mismatch
-    local u1 = Unicode.utf8_to_utf16(current_text, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
+    local current_u16 = Unicode.utf8_to_utf16(current_text, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
     ---@type table<number>
     ---@diagnostic disable-next-line: assign-type-mismatch
-    local u2 = Unicode.utf8_to_utf16(M.last.text, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
+    local last_u16 = Unicode.utf8_to_utf16(M.last.text, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
 
-    Log.debug('u1 = {}', u1)
-    Log.debug('u2 = {}', u2)
-
-    local n = 0
-    for i = 1, math.min(#u1, #u2) do
-        if u1[i] == u2[i] then
-            n = n + 1
+    local plen = 0
+    for i = 1, math.min(#current_u16, #last_u16) do
+        if current_u16[i] == last_u16[i] then
+            plen = plen + 1
         else
             break
         end
     end
-    local i = 0
-    while i + n < math.min(#u1, #u2) do
-        if u1[#u1 - i] == u2[#u2 - i] then
-            i = i + 1
+    local slen = 0
+    while slen + plen < math.min(#current_u16, #last_u16) do
+        if current_u16[#current_u16 - slen] == last_u16[#last_u16 - slen] then
+            slen = slen + 1
         else
             break
         end
     end
-    Log.debug('n = {}, i = {}', n, i)
 
-    local lu32 = vim.list_slice(u1, 1, n)
-    local ru32 = vim.list_slice(u1, #u1 - i + 1, #u1)
-    local diffu32 = vim.list_slice(u1, n + 1, #u1 - i)
-    Log.debug('lu32 = {}', lu32)
-    Log.debug('ru32 = {}', ru32)
-    Log.debug('diffu32 = {}', diffu32)
+    local lu16 = vim.list_slice(current_u16, 1, plen)
+    local ru16 = vim.list_slice(current_u16, #current_u16 - slen + 1, #current_u16)
+    local diffu16 = vim.list_slice(current_u16, plen + 1, #current_u16 - slen)
 
-    local lu8 = Unicode.utf16_to_utf8(lu32, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
-    local ru8 = Unicode.utf16_to_utf8(ru32, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
-    local diffu8 = Unicode.utf16_to_utf8(diffu32, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
-    Log.debug('lu8 = {}', lu8)
-    Log.debug('ru8 = {}', ru8)
-    Log.debug('diffu8 = {}', diffu8)
+    local lu8 = Unicode.utf16_to_utf8(lu16, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
+    local ru8 = Unicode.utf16_to_utf8(ru16, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
+    local diffu8 = Unicode.utf16_to_utf8(diffu16, Unicode.ENDIAN.LE, Unicode.FORMAT.UNIT)
 
     local diff_meta = {
-        plen = n,
-        slen = i,
+        plen = plen,
+        slen = slen,
         bplen = #lu8,
         bslen = #ru8,
         pmd5 = M.last.ciphertext,
