e4af340b59e2ad9135e83ed2b43a601a138b5013
luozhiya <luozhiya@petalmail.com>
2025-06-15 15:34:54 +0800

    2025/06/15

--- tests/functional/fn/diff.lua
+++ tests/functional/fn/diff.lua
@@ -4,19 +4,19 @@ describe('fn.diff', function()
     it('diff', function()
         -- 准备测试数据（包含中英文字符）
         local old_text = {
-        "Hello world!",
-        "这是一行中文文本",
-        "Line 3: To be deleted",
-        "Another line with 中文",
-        "Final line"
+            'Hello world!',
+            '这是一行中文文本',
+            'Line 3: To be deleted',
+            'Another line with 中文',
+            'Final line'
         }
 
         local new_text = {
-        "Hello world!",  -- 未修改
-        "这是一行修改后的中文文本",  -- 修改
-        "New line inserted",  -- 新增
-        "Another line with 中文",  -- 未修改
-        "Final line with changes"  -- 修改
+            'Hello world!', -- 未修改
+            '这是一行修改后的中文文本', -- 修改
+            'New line inserted', -- 新增
+            'Another line with 中文', -- 未修改
+            'Final line with changes' -- 修改
         }
 
         -- 计算差异
@@ -29,25 +29,25 @@ describe('fn.diff', function()
     it('vim.diff', function()
         -- 准备测试数据（包含中英文字符）
         local old_lines = {
-        "Hello world!",
-        "这是一行中文文本",
-        "Line 3: To be deleted",
-        "Another line with 中文",
-        "Final line"
+            'Hello world!',
+            '这是一行中文文本',
+            'Line 3: To be deleted',
+            'Another line with 中文',
+            'Final line'
         }
 
         local new_lines = {
-        "Hello world!",  -- 未修改
-        "这是一行修改后的中文文本",  -- 修改
-        "New line inserted",  -- 新增
-        "Another line with 中文1",  -- 未修改
-        "Final line with changes"  -- 修改
+            'Hello world!', -- 未修改
+            '这是一行修改后的中文文本', -- 修改
+            'New line inserted', -- 新增
+            'Another line with 中文1', -- 未修改
+            'Final line with changes' -- 修改
         }
 
-        local old_text = table.concat(old_lines, "\n")
-        local new_text = table.concat(new_lines, "\n")
+        local old_text = table.concat(old_lines, '\n')
+        local new_text = table.concat(new_lines, '\n')
         local diff = vim.diff(old_text, new_text, {
-            result_type = "indices",
+            result_type = 'indices',
             linematch = true
         })
         print(vim.inspect(diff))
