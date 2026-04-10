1b56b82bca89f7a4e109052df723ec15440edd23
luozhiya <luozhiya@petalmail.com>
2026-04-10 09:37:36 +0800

    Filter events before sync_state

--- lua/fittencode/inline/controller.lua
+++ lua/fittencode/inline/controller.lua
@@ -119,6 +119,11 @@ function Controller:_initialize(options)
         group = vim.api.nvim_create_augroup('FittenCode.Inline.ServiceUpdate', { clear = true }),
         pattern = '*',
         callback = function(args)
+            local filters = { 'BufFilePost', 'FileType' }
+            local current_buf = vim.api.nvim_get_current_buf()
+            if vim.tbl_contains(filters, args.event) and current_buf ~= args.buf then
+                return
+            end
             self:sync_state()
         end,
     })
